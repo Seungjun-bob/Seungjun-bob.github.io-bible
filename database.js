cbfebe6a2f4a98355d2bbf15f4cc768eddcb94b6
Seungjun <rkdtmdwns0105@gmail.com>
2024-07-30 23:54:42 +0900

    .gitignore

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const path = require('path');
 const dbPath = path.join(__dirname, 'db', 'data');
 
 // 데이터베이스 연결
-const db = new Database(dbPath, { verbose: console.log });
+const db = new Database(dbPath);
 
 // 쿼리 실행 함수
 function runQuery(query, params = []) {
