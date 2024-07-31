20dbee178a82658c667a8e11d575b89ddffe761e
Seungjun <rkdtmdwns0105@gmail.com>
2024-08-01 01:04:42 +0900

    .

--- database.js
+++ database.js
@@ -1,25 +1,42 @@
-const Database = require('better-sqlite3');
+const mysql = require('mysql2/promise');
 const path = require('path');
-// 데이터베이스 파일 경로 설정
-const dbPath = path.join(__dirname, 'db', 'data');
+require('dotenv').config();
 
-// 데이터베이스 연결
-const db = new Database(dbPath);
+// 커넥션 풀 생성
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,             // MariaDB 기본 포트
+  dateStrings: 'date',
+  connectionLimit: 1000,    // 최대 커넥션 수
+  multipleStatements: true  // 여러 개의 SQL 문을 실행할 수 있도록 허용
+});
 
 // 쿼리 실행 함수
-function runQuery(query, params = []) {
+async function runQuery(query, params = []) {
+  let connection;
   try {
-    return db.prepare(query).all(params);
+    connection = await pool.getConnection();
+    const [rows, fields] = await connection.execute(query, params);
+    return rows;
   } catch (err) {
     console.error('쿼리 실행 오류:', err.message);
     throw err; // 또는 적절한 오류 처리를 수행하세요
+  } finally {
+    if (connection) connection.release(); // 커넥션을 풀에 반환합니다
   }
 }
 
 // 데이터베이스 종료 함수
-function closeDb() {
-  db.close();
-  console.log('데이터베이스 연결이 종료되었습니다.');
+async function closeDb() {
+  try {
+    await pool.end();
+    console.log('데이터베이스 연결이 종료되었습니다.');
+  } catch (err) {
+    console.error('커넥션 풀 종료 오류:', err.message);
+  }
 }
 
 // 모듈 내보내기
--- routes/home.js
+++ routes/home.js
@@ -10,13 +10,13 @@ const db = require('../database');
 
 // 쿼리 실행 예제
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
     // 접속 자의 IP 주소를 가져옵니다, 접속 시간도 함께 로깅합니다.
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const time = new Date().toISOString();
     logger.info(`[${time}] ${ip} GET /`);
 
-    const rows = db.runQuery('SELECT * FROM bible');
+    const rows = await db.runQuery('SELECT * FROM bible');
 
 	res.render('home', { 
         verses: rows
